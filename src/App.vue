0455bcd505c3445892825bd2fdbdde87b5194341
Luke <lsmaclean0@frostburg.edu>
2021-04-13 13:02:49 -0400

    todo list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
 </template>
 
 <script>
+
+import Todos from './components/Todos';
+
 export default {
   name: "App",
   components: {
-    
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ],
+    };
   },
 };
 </script>
